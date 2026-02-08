de3d43ee9b80cd17084c86e16e451a8fd1c5f4be
Daniel Ferreira <daniel.ferreira.dev@gmail.com>
2026-02-07 23:58:42 -0300

    fix: interval to check health

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,9 +56,9 @@ resource "aws_ecs_task_definition" "framedrop_upload_app" {
 
     healthCheck = {
       command     = ["CMD-SHELL", "wget --no-verbose --tries=1 --spider http://localhost:8080/actuator/health || exit 1"]
-      interval    = 30
+      interval    = 60
       timeout     = 5
-      retries     = 3
+      retries     = 5
       startPeriod = 60
     }
 
